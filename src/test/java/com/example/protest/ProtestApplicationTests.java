5e78306675101e2171c52c8770956b185c24120c
AtoZ990 <worktest990@gmail.com>
2022-06-19 20:55:41 +0700

    fisrt

---
+++ src/test/java/com/example/protest/ProtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

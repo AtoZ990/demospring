f882f61dacae7cb293247614f1741111185e6588
AtoZ990 <worktest990@gmail.com>
2022-08-14 19:06:30 +0700

    12

--- src/main/java/com/example/protest/test01.java
+++ src/main/java/com/example/protest/test01.java
@@ -1,24 +1,5 @@
 package com.example.protest;
 
 public class test01 {
-    int t;
-    int x;
-    int y;
-    int t02;
-    int t01;
-    int t03;
-    int t05;
-    int t06;
-    int t07;
-    int r01;
-    int p01;
-    int p04;
-    int p05;
-    int p06;
-    int p03;
-    int p09;
-    int p07;
-    int p11;
-    int p10;
-    int p12;
+
 }

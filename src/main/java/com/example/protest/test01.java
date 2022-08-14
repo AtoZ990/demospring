1b7e5a36249085425649e589dd0a88ee65c5968a
AtoZ990 <worktest990@gmail.com>
2022-08-14 19:17:17 +0700

    Revert "6"
    This reverts commit e9cf9d15ee1a137107ef53d9696ea8227024bf3d.

--- src/main/java/com/example/protest/test01.java
+++ src/main/java/com/example/protest/test01.java
@@ -3,6 +3,7 @@ package com.example.protest;
 public class test01 {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     int uupi = 0;
 =======
     int ui = 0;
@@ -10,5 +11,8 @@ public class test01 {
 =======
     int ui = 0;
 >>>>>>> 69336666fc5fc861603bbcf9b4b80853b56fdb8e
+=======
+    int uupi = 0;
+>>>>>>> parent of e9cf9d1 (6)
 
 }

f1111195cfae41dd0b2ad2b51724bceaa5719a3d
AtoZ990 <worktest990@gmail.com>
2022-08-14 19:18:02 +0700

    99

--- src/main/java/com/example/protest/test01.java
+++ src/main/java/com/example/protest/test01.java
@@ -1,18 +1,5 @@
 package com.example.protest;
 
 public class test01 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    int uupi = 0;
-=======
-    int ui = 0;
->>>>>>> 69336666fc5fc861603bbcf9b4b80853b56fdb8e
-=======
-    int ui = 0;
->>>>>>> 69336666fc5fc861603bbcf9b4b80853b56fdb8e
-=======
-    int uupi = 0;
->>>>>>> parent of e9cf9d1 (6)
 
 }
